5f50023023cc5db3471ec2af6b1d9640004e78f4
Yoitsjae <ncubejason896@gmail.com>
2024-03-07 19:49:25 +0200

    0x00-pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,38 @@
 def pascal_triangle(n):
     """
-    Generate Pascal's Triangle up to the nth row.
-    
+    Generates Pascal's triangle up to the nth row.
+
     Args:
         n (int): The number of rows to generate.
-        
+
     Returns:
-        list of lists: Pascal's Triangle up to the nth row.
+        list of lists: A list of lists representing Pascal's triangle.
     """
-    triangle = []
-    
     if n <= 0:
-        return triangle
-    
-    for i in range(n):
-        row = [1] * (i + 1)
-        for j in range(1, len(row) - 1):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
-    
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+
     return triangle
 
-# Test the function
-if __name__ == "__main__":
-    triangle = pascal_triangle(5)
+
+def print_triangle(triangle):
+    """
+    Prints Pascal's triangle.
+
+    Args:
+        triangle (list of lists): Pascal's triangle to print.
+    """
     for row in triangle:
-        print(row)
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
