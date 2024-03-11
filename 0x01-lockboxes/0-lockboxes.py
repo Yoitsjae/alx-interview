4c2303f41a480b7c53080b21f0eb75e6aa16e7c6
Yoitsjae <ncubejason896@gmail.com>
2024-03-11 14:11:09 +0200

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Determines if all the boxes can be opened.
+"""
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened."""
+    if not boxes:
+        return False
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    return len(keys) == len(boxes)
+
+if __name__ == "__main__":
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
