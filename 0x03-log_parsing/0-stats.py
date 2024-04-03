4c97db37836e232e0d06d5be3e9df6362bfe42e5
Yoitsjae <ncubejason896@gmail.com>
2024-04-03 13:43:58 +0200

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,41 @@
 #!/usr/bin/python3
 import sys
-import signal
 
-def signal_handler(sig, frame):
-    print_stats()
+class LogParser:
+    def __init__(self):
+        self.total_file_size = 0
+        self.status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+        self.line_count = 0
 
-def print_stats():
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
-
-total_size = 0
-status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-
-def parse_line(line):
-    global total_size
-    try:
+    def parse_line(self, line):
         parts = line.split()
-        ip_address = parts[0]
-        status_code = parts[-2]
-        file_size = int(parts[-1])
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-        total_size += file_size
-    except Exception as e:
-        pass
+        if len(parts) >= 7:
+            status_code = parts[-2]
+            file_size = parts[-1]
+            if status_code in self.status_codes:
+                self.status_codes[status_code] += 1
+            try:
+                self.total_file_size += int(file_size)
+            except ValueError:
+                pass
 
-def main():
-    line_count = 0
-    signal.signal(signal.SIGINT, signal_handler)
+    def print_stats(self):
+        print("File size: {}".format(self.total_file_size))
+        for code in sorted(self.status_codes.keys()):
+            if self.status_codes[code] > 0:
+                print("{}: {}".format(code, self.status_codes[code]))
 
-    for line in sys.stdin:
-        line_count += 1
-        parse_line(line.strip())
-
-        if line_count % 10 == 0:
-            print_stats()
-
-    print_stats()
+    def process_logs(self):
+        try:
+            for line in sys.stdin:
+                self.line_count += 1
+                self.parse_line(line.strip())
+                if self.line_count % 10 == 0:
+                    self.print_stats()
+        except KeyboardInterrupt:
+            pass
+        self.print_stats()
 
 if __name__ == "__main__":
-    main()
+    parser = LogParser()
+    parser.process_logs()
