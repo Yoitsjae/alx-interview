a7417616a18669fcd44a0c29d0c0d546c993b957
Yoitsjae <ncubejason896@gmail.com>
2024-05-02 14:59:02 +0200

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -49,25 +49,25 @@ class NQueen:
 
 
 # Main
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
 
-    N = sys.argv[1]
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
-    try:
-        N = int(N)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+N = sys.argv[1]
 
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+try:
+    N = int(N)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
-    queen = NQueen(N)
-    res = queen.nQueen(1)
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
-    for i in res:
-        print(i)
+queen = NQueen(N)
+res = queen.nQueen(1)
+
+for i in res:
+    print(i)
