f2c42acecc19861a4803126ff1216a995c1bcb95
Yoitsjae <ncubejason896@gmail.com>
2024-05-02 15:02:17 +0200

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,55 +3,62 @@
 import sys
 
 
-class NQueen:
-    """ Class for solving N Queen Problem """
+class NQueenSolver:
+    """ Class for solving the N Queens problem """
 
     def __init__(self, n):
-        """ Initialize the NQueen class """
-        self.n = n
-        self.x = [0] * (n + 1)
-        self.res = []
+        """ Initialize the NQueenSolver class """
+        self.board_size = n
+        self.columns = [0] * (n + 1)
+        self.solutions = []
 
-    def place(self, k, i):
-        """ Check if a queen can be placed in a specific position """
-        for j in range(1, k):
-            if self.x[j] == i or abs(self.x[j] - i) == abs(j - k):
+    def can_place_queen(self, row, column):
+        """ Check if a queen can be placed at the specified position """
+        for prev_row in range(1, row):
+            prev_col = self.columns[prev_row]
+            if prev_col == column or abs(prev_col - column) == abs(prev_row - row):
                 return False
         return True
 
-    def nQueen(self, k):
-        """ Solve the N Queen problem recursively """
-        for i in range(1, self.n + 1):
-            if self.place(k, i):
-                self.x[k] = i
-                if k == self.n:
-                    solution = []
-                    for j in range(1, self.n + 1):
-                        solution.append([j - 1, self.x[j] - 1])
-                    self.res.append(solution)
+    def solve_n_queens(self, row):
+        """ Recursively solve the N Queens problem """
+        for col in range(1, self.board_size + 1):
+            if self.can_place_queen(row, col):
+                self.columns[row] = col
+                if row == self.board_size:
+                    self.add_solution()
                 else:
-                    self.nQueen(k + 1)
-        return self.res
+                    self.solve_n_queens(row + 1)
+
+    def add_solution(self):
+        """ Add the current solution to the list of solutions """
+        solution = [(row - 1, self.columns[row] - 1) for row in range(1, self.board_size + 1)]
+        self.solutions.append(solution)
+
+    def get_solutions(self):
+        """ Return the list of solutions """
+        return self.solutions
 
 
 if __name__ == "__main__":
+    # Validate command-line arguments
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+        print("Usage: nqueens.py N")
         sys.exit(1)
 
     try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
+        board_size = int(sys.argv[1])
+        if board_size < 4:
+            raise ValueError("N must be at least 4")
+    except ValueError as e:
+        print("Invalid input:", e)
         sys.exit(1)
 
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+    # Solve the N Queens problem
+    solver = NQueenSolver(board_size)
+    solver.solve_n_queens(1)
+    solutions = solver.get_solutions()
 
-    queen = NQueen(N)
-    res = queen.nQueen(1)
-
-    for solution in res:
+    # Print solutions
+    for solution in solutions:
         print(solution)
-
