66983db95f8c4126b3603b5d6c3ba5d220c48d0b
Yoitsjae <ncubejason896@gmail.com>
2024-08-10 12:03:41 +0200

    Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,42 +2,51 @@
 
 """ Prime Game """
 
-def sieve_of_eratosthenes(max_num):
-    """ Generate a list of primes up to max_num using the Sieve of Eratosthenes """
-    is_prime = [True] * (max_num + 1)
-    p = 2
-    while (p * p <= max_num):
-        if (is_prime[p] == True):
-            for i in range(p * p, max_num + 1, p):
-                is_prime[i] = False
-        p += 1
-    return [p for p in range(2, max_num + 1) if is_prime[p]]
-
 def isWinner(x, nums):
-    """ Determine the winner of the prime game after x rounds with different n values """
-    if x < 1 or not nums:
+    if not nums or x < 1:
         return None
     
+    # Find the maximum number in nums to limit our sieve and DP array
     max_num = max(nums)
-    primes = sieve_of_eratosthenes(max_num)
-    prime_count = [0] * (max_num + 1)
-
-    for i in range(1, max_num + 1):
-        prime_count[i] = prime_count[i - 1]
-        if i in primes:
-            prime_count[i] += 1
     
-    score = {"Maria": 0, "Ben": 0}
-
-    for n in nums:
-        if prime_count[n] % 2 == 1:
-            score["Maria"] += 1
+    # Sieve of Eratosthenes to find all primes up to max_num
+    is_prime = [True] * (max_num + 1)
+    is_prime[0] = is_prime[1] = False
+    
+    for start in range(2, int(max_num**0.5) + 1):
+        if is_prime[start]:
+            for multiple in range(start*start, max_num + 1, start):
+                is_prime[multiple] = False
+    
+    # Precompute winners for every number up to max_num
+    winner = [0] * (max_num + 1)
+    
+    for n in range(1, max_num + 1):
+        primes = [i for i in range(1, n + 1) if is_prime[i]]
+        if not primes:
+            winner[n] = 1  # Ben wins if there are no primes
         else:
-            score["Ben"] += 1
-
-    if score["Maria"] > score["Ben"]:
-        return "Maria"
-    elif score["Ben"] > score["Maria"]:
-        return "Ben"
+            for prime in primes:
+                if n % prime == 0 and winner[n - prime] == 0:
+                    winner[n] = 1
+                    break
     
-    return None
+    # Count the wins for Maria and Ben
+    maria_wins = 0
+    ben_wins = 0
+    
+    for n in nums:
+        if winner[n] == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+    
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
+
+# Test case
+print("Winner: {}".format(isWinner(3, [4, 5, 1])))  # Example from the problem statement
